fb3d1f7ec41a469743f0e36d0564284a21baf38a
Serhii-Zamorylo23 <zamorylo@i.ua>
2024-09-12 15:48:38 +0300

    //

--- task-5.js
+++ task-5.js
@@ -1,40 +1,40 @@
-class Car{
-  constructor({price,maxSpeed}){
-    this.speed=0
-    this.price=price
-    this.maxSpeed=maxSpeed
-    this.isOn=false
-    this.distance=0
+class Car {
+  constructor({ price, maxSpeed }) {
+    this.speed = 0
+    this.price = price
+    this.maxSpeed = maxSpeed
+    this.isOn = false
+    this.distance = 0
   }
-  static getSpecs(car){
+  static getSpecs(car) {
     console.log(`Максимальна швидкість: ${car.maxSpeed} швидкість на даний момент: ${car.speed} Двигун: ${car.isOn} дистанція: ${car.distance} ціна: ${car.price}`)
   }
-  get price(){
+  get price() {
     return this._price
   }
-  set price(Price){
-    this._price=Price
+  set price(Price) {
+    this._price = Price
   }
   turnOn() {
-    this.isOn=true
+    this.isOn = true
   }
   turnOff() {
-    this.isOn=false
-    this.speed=0
+    this.isOn = false
+    this.speed = 0
   }
   accelerate(value) {
-    if(this.speed + value <=this.maxSpeed){
-      this.speed+=value
+    if (this.speed + value <= this.maxSpeed) {
+      this.speed += value
     }
   }
   decelerate(value) {
-    if(this.speed - value >=0){
-      this.speed-=value
-    } 
+    if (this.speed - value >= 0) {
+      this.speed -= value
+    }
   }
   drive(hours) {
-    if(this.isOn==true){
-      this.distance += hours*this.speed
+    if (this.isOn == true) {
+      this.distance += hours * this.speed
     }
   }
 }
